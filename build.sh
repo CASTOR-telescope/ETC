36895ceb5a44518cef69e9dfb69a726823aaa8b0
Isaac Cheng <isaac.cheng.ca@gmail.com>
2022-01-10 21:35:45 -0500

    Docker image still not working on CANFAR...

--- build.sh
+++ build.sh
@@ -14,10 +14,22 @@ source Docker_env
 #
 # Build the project
 #
-docker build --build-arg NOTEBOOK_DIR=${NOTEBOOK_DIR} \
-             --build-arg NB_USER=${NB_USER} \
-             -t castor_etc:${VERSION} \
-             -f ${SCRIPT_DIR}/Dockerfile.${CUSTOMIZE_ENV}CustomEnv .
+if [[ "$CUSTOMIZE_ENV" = "yes" ]]
+then
+    echo "Building with custom JupyterLab environment"
+    docker build --build-arg NOTEBOOK_DIR=${NOTEBOOK_DIR} \
+                 --build-arg NB_USER=${NB_USER} \
+                 -t castor_etc:${VERSION} \
+                 -f ${SCRIPT_DIR}/Dockerfile.yesCustomEnv .
+elif [[ "$CUSTOMIZE_ENV" == "no" ]]
+then
+    echo "Building with default JupyterLab environment"
+    docker build -t castor_etc:${VERSION} \
+                 -f ${SCRIPT_DIR}/Dockerfile.noCustomEnv .
+else
+    echo "ERROR: CUSTOMIZE_ENV is must be 'yes' or 'no'"
+    exit 1
+fi
 #
 echo "Finishing building castor_etc:${VERSION}"
 echo "Now running castor_etc_v${VERSION}..."
