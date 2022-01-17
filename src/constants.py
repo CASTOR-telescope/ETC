4ce932e68f460197224fbcc5c7c5ae7ab479896a
Isaac Cheng <isaac.cheng.ca@gmail.com>
2022-01-17 14:45:43 -0500

    update NOTEBOOK_DIR

--- src/constants.py
+++ src/constants.py
@@ -24,7 +24,7 @@ BASEPATH = dirname(dirname(abspath(__file__))) + "/"
 
 # The directory to store output files. This directory must already exist and be
 # discoverable by the container (e.g., via the "-v" bind mount for a local Docker build)
-OUTPATH = "/arc/home/IsaacCheng/ETC_plots/"  # ! CHANGE ME !
+OUTPATH = "/arc/home/IsaacCheng/CASTOR/ETC_plots/"  # ! CHANGE ME !
 
 # The directory containing the data files (e.g., passbands, sky background, etc.)
 DATAPATH = BASEPATH + "data/"
