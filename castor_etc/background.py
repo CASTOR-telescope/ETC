7ca38d4dc9b9957c80bf31d77e3cf2564c89d2b0
Isaac Cheng <isaac.cheng.ca@gmail.com>
2022-04-01 06:32:15 -0400

    Added license header

--- castor_etc/background.py
+++ castor_etc/background.py
@@ -1,5 +1,68 @@
 """
-Sky background utilities.
+Utilities to characterize the sky background (Earthshine, zodiacal light, geocoronal
+emission).
+
+---
+
+        GNU General Public License v3 (GNU GPLv3)
+
+(c) 2022.                            (c) 2022.
+Government of Canada                 Gouvernement du Canada
+National Research Council            Conseil national de recherches
+Ottawa, Canada, K1A 0R6              Ottawa, Canada, K1A 0R6
+All rights reserved                  Tous droits réservés
+
+NRC disclaims any warranties,        Le CNRC dénie toute garantie
+expressed, implied, or               énoncée, implicite ou légale,
+statutory, of any kind with          de quelque nature que ce
+respect to the software,             soit, concernant le logiciel,
+including without limitation         y compris sans restriction
+any warranty of merchantability      toute garantie de valeur
+or fitness for a particular          marchande ou de pertinence
+purpose. NRC shall not be            pour un usage particulier.
+liable in any event for any          Le CNRC ne pourra en aucun cas
+damages, whether direct or           être tenu responsable de tout
+indirect, special or general,        dommage, direct ou indirect,
+consequential or incidental,         particulier ou général,
+arising from the use of the          accessoire ou fortuit, résultant
+software. Neither the name           de l'utilisation du logiciel. Ni
+of the National Research             le nom du Conseil National de
+Council of Canada nor the            Recherches du Canada ni les noms
+names of its contributors may        de ses  participants ne peuvent
+be used to endorse or promote        être utilisés pour approuver ou
+products derived from this           promouvoir les produits dérivés
+software without specific prior      de ce logiciel sans autorisation
+written permission.                  préalable et particulière
+                                     par écrit.
+
+This file is part of the             Ce fichier fait partie du projet
+FORECASTOR ETC project.              FORECASTOR ETC.
+
+FORECASTOR ETC is free software:     FORECASTOR ETC est un logiciel
+you can redistribute it and/or       libre ; vous pouvez le redistribuer
+modify it under the terms of         ou le modifier suivant les termes de
+the GNU General Public               la "GNU General Public
+License as published by the          License" telle que publiée
+Free Software Foundation,            par la Free Software Foundation :
+either version 3 of the              soit la version 3 de cette
+License, or (at your option)         licence, soit (à votre gré)
+any later version.                   toute version ultérieure.
+
+FORECASTOR ETC is distributed        FORECASTOR ETC est distribué
+in the hope that it will be          dans l'espoir qu'il vous
+useful, but WITHOUT ANY WARRANTY;    sera utile, mais SANS AUCUNE
+without even the implied warranty    GARANTIE : sans même la garantie
+of MERCHANTABILITY or FITNESS FOR    implicite de COMMERCIALISABILITÉ
+A PARTICULAR PURPOSE. See the        ni d'ADÉQUATION À UN OBJECTIF
+GNU General Public License for       PARTICULIER. Consultez la Licence
+more details.                        Générale Publique GNU pour plus
+                                     de détails.
+
+You should have received             Vous devriez avoir reçu une
+a copy of the GNU General            copie de la Licence Générale
+Public License along with            Publique GNU avec FORECASTOR ETC ;
+FORECASTOR ETC. If not, see          si ce n'est pas le cas, consultez :
+<http://www.gnu.org/licenses/>.      <http://www.gnu.org/licenses/>.
 """
 
 from copy import deepcopy
@@ -81,7 +144,6 @@ class Background:
             The sky background AB magnitudes per square arcsecond (incl. Earthshine &
             zodiacal light) in each of the telescope's passbands.
 
-
         Returns
         -------
           background :: `Background` object
@@ -159,6 +221,7 @@ class Background:
           linewidth :: int or float or `astropy.Quantity` length
             The linewidth of the geocoronal emission line. If an int or float, it is
             assumed to be in angstrom.
+            This is currently not used in any calculations.
 
         Attributes
         ----------
@@ -170,6 +233,7 @@ class Background:
 
           geo_linewidth :: list of floats
             The linewidths of the geocoronal emission lines in angstrom.
+            This is currently not used in any calculations.
 
         Returns
         -------
@@ -197,7 +261,7 @@ class Background:
                 raise TypeError("geo_linewidth must be an `astropy.Quantity` length.")
         self.geo_flux.append(flux)
         self.geo_wavelength.append(wavelength)
-        self.geo_linewidth.append(linewidth)
+        self.geo_linewidth.append(linewidth)  # currently not used in any calculations
 
     def calc_mags_per_sq_arcsec(self, TelescopeObj):
         """
@@ -259,4 +323,10 @@ class Background:
                 wavelengths=TelescopeObj.passband_pivots[band],
             )[0]
 
+        # REVIEW:
+        # At this point, although the sky background magnitudes are technically just AB
+        # magnitudes (converted from erg/s/cm^2/A), these sky backgrounds only make sense
+        # if they are in units of AB mags per square arcsecond (recall the sky background
+        # values assumed by Dr. Patrick Côté). Thus we will just assume these values are
+        # in AB mags per sq. arcsec...
         self.mags_per_sq_arcsec = avg_sky_mags
