294d5fe6bf956b2f28a18599acb8350532c6ed03
Dhananjhay <dhananjhay03@gmail.com>
2023-08-25 15:18:21 -0700

    Revert "paths to stellar_models folder according to the build instructions"
    This reverts commit 6590c38679d1c6e02d2f091658c152b580b146b6.

--- castor_etc/spectrum.py
+++ castor_etc/spectrum.py
@@ -103,14 +103,9 @@ from astropy.coordinates import SkyCoord
 from astropy.constants import R_sun, pc
 from astropy.wcs import WCS
 
-# Use local_path when working with castor_etc on your local machine.
-# local_path = <path to local stellar_models directory> 
-
-# Use local_docker_path when building docker container locally.
-# local_docker_path = join(DATAPATH,"transit_data/stellar_models")
-
-# Use canfar_docker_path when building castor_etc package ready to be deployed on the canfar server.
-canfar_docker_path = "/arc/projects/CASTOR/stellar_models"
+# Change the path variable according to the build instructions.
+path = '/Users/dhananjhaybansal/Applications/POET/stellar_models/'
+#path = '/opt/mypackage/castor_etc/data/transit_data/stellar_models/'
 
 def getStarData(temperature, metallicity, logg, 
                     model_grid='ATLAS9',
@@ -141,12 +136,12 @@ def getStarData(temperature, metallicity, logg,
     # Default to ATLAS9 model
     #     If Teff < 3500 K (i.e., min Teff in ATLAS9 grid, switch to BTSettl grid)
     if ( (model_grid == 'BTSettl') | (temperature < 3500) ):
-        if not os.path.exists(os.path.join(canfar_docker_path,"BTSettl_CIFIST/")):
+        if not os.path.exists(os.path.join(path,"BTSettl_CIFIST/")):
             raise RuntimeError(
-                f"The specified directory path `{canfar_docker_path}` does not contain the stellar_models directory." + "Please check the path variable."
+                f"The specified directory path `{path}` does not contain the stellar_models directory." + "Please check the path variable."
             )
         else:
-          grid_dir = os.path.join(canfar_docker_path,"BTSettl_CIFIST/")
+          grid_dir = os.path.join(path,"BTSettl_CIFIST/")
 
         # Scan directory to inventory model grid
         # Assumes all files starting with 'lte' are model files
@@ -189,12 +184,12 @@ def getStarData(temperature, metallicity, logg,
         flux = d[:,1] # [erg/s/cm2/angstrom]
 
     elif model_grid == 'ATLAS9':
-        if not os.path.exists(os.path.join(canfar_docker_path, "ATLAS9/ck04models/")):
+        if not os.path.exists(os.path.join(path, "ATLAS9/ck04models/")):
             raise RuntimeError(
-                f"The specified directory path `{canfar_docker_path}` does not contain the stellar_model directory." + "Please modify the path variable. "
+                f"The specified directory path `{path}` does not contain the stellar_model directory." + "Please modify the path variable. "
             )
         else:
-            grid_dir = os.path.join(canfar_docker_path, "ATLAS9/ck04models/")
+            grid_dir = os.path.join(path, "ATLAS9/ck04models/")
 
         teff_grid = np.array([3500, 3750, 4000, 4250, 4500, 4750, 5000, 5250, 5500, 5750, 
                               6000, 6250, 6500, 6750, 7000, 7250, 7500, 7750, 8000, 8250, 
